6e88bb9d919067cad13fdc5c8a8e7203e1cd17a8
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2025-01-20 16:11:02 -0300

    Init projeto desafio de pontos de interesse por gps

---
+++ pontos-localizacao/src/main/java/dev/correa/gps/PontosLocalizacaoApplication.java
@@ -0,0 +1,13 @@
+package dev.correa.gps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontosLocalizacaoApplication {
+	
+	public static void main (String[] args) {
+		SpringApplication.run(PontosLocalizacaoApplication.class, args);
+	}
+	
+}
---
+++ pontos-localizacao/src/test/java/dev/correa/gps/PontosLocalizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.correa.gps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontosLocalizacaoApplicationTests {
+	
+	@Test
+	void contextLoads () {
+	}
+	
+}
