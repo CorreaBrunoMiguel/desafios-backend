b7c1e7be42dcc3a4872b60a4011b58d0ee3322aa
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2025-01-23 09:32:01 -0300

    Init

---
+++ sala-reunioes/src/main/java/dev/correa/reunioes/SalaReunioesApplication.java
@@ -0,0 +1,13 @@
+package dev.correa.reunioes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaReunioesApplication {
+	
+	public static void main (String[] args) {
+		SpringApplication.run(SalaReunioesApplication.class, args);
+	}
+	
+}
---
+++ sala-reunioes/src/test/java/dev/correa/reunioes/SalaReunioesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.correa.reunioes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaReunioesApplicationTests {
+	
+	@Test
+	void contextLoads () {
+	}
+	
+}
