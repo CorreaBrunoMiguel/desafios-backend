3467277cd031cef707e235f670017fd7247a9f98
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2025-01-27 09:56:19 -0300

    criado os repositories para Sala e Reserva

---
+++ sala-reunioes/src/main/java/dev/correa/reunioes/repository/ReservaRepository.java
@@ -0,0 +1,13 @@
+package dev.correa.reunioes.repository;
+
+import dev.correa.reunioes.model.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Author: Bruno Miguel Correa
+ * Email: devsistemascorrea@gmail.com
+ * Data: 27/01/2025
+ **/
+
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+}
